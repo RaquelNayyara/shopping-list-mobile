6883cdd01a807aac4666893aaed2578ae3faf36e
RaquelNayyara <quelaazzz@gmail.com>
2023-11-08 15:31:27 +0700

    hapus TODO

--- shoppinglist/lib/screens/shoplist_form.dart
+++ shoppinglist/lib/screens/shoplist_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// TODO: Impor drawer yang sudah dibuat sebelumnya
 import 'package:shoppinglist/widgets/left_drawer.dart';
 
 class ShopFormPage extends StatefulWidget {
@@ -27,7 +26,6 @@ class _ShopFormPageState extends State<ShopFormPage> {
           backgroundColor: Colors.indigo,
           foregroundColor: Colors.white,
         ),
-        // TODO: Tambahkan drawer yang sudah dibuat di sini
         body: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -67,7 +65,6 @@ class _ShopFormPageState extends State<ShopFormPage> {
                         borderRadius: BorderRadius.circular(5.0),
                       ),
                     ),
-                    // TODO: Tambahkan variabel yang sesuai
                     onChanged: (String? value) {
                       setState(() {
                         _price = int.parse(value!);
@@ -96,7 +93,6 @@ class _ShopFormPageState extends State<ShopFormPage> {
                     ),
                     onChanged: (String? value) {
                       setState(() {
-                        // TODO: Tambahkan variabel yang sesuai
                         _description = value!;
                       });
                     },
@@ -130,7 +126,6 @@ class _ShopFormPageState extends State<ShopFormPage> {
                                         CrossAxisAlignment.start,
                                     children: [
                                       Text('Nama: $_name'),
-                                      // TODO: Munculkan value-value lainnya
                                       Text('Harga: $_price'),
                                       Text('Deskripsi: $_description'),
                                     ],
